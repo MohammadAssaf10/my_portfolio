a0b6c9526de7d697e4015272dd88b583a6848a4a
Mohammadassaf10 <mhamad234assaf@gmail.com>
2025-01-03 15:06:03 +0300

    Deploy

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,5 @@
 class Routes {
   Routes._();
 
-  static const String mainPage = '/MAPortfolio';
+  static const String mainPage = '/';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'my_portfolio_app.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const MyPortfolioApp());
 }
