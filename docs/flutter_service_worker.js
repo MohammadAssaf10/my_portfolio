39ec5b194f1952fdcc27f89dd97bbb1feac874ac
Mohammadassaf10 <mhamad234assaf@gmail.com>
2025-01-03 14:21:39 +0300

    Deploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -48,7 +48,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a9aa94ea18c0cb243cc7c3e615653e57
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "9d92dd2cbaadd97deaf84f797a73b614",
+"flutter_bootstrap.js": "0900466676850022f5710ef85e6c628f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -56,7 +56,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a9aa94ea18c0cb243cc7c3e615653e57
 "index.html": "0406e2b061a53c48fe2f80d911587fc2",
 "/": "0406e2b061a53c48fe2f80d911587fc2",
 "loader.gif": "b13646f6e6c50ba78579170183b56b5f",
-"main.dart.js": "e675b732a85e4805bf2843ee3ea95c45",
+"main.dart.js": "a2182cfe2a7881578f3d1c4333d50ba5",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"};
 // The application shell files that are downloaded before a service worker can
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
 import 'my_portfolio_app.dart';
 
 void main() {
-  usePathUrlStrategy();
   runApp(const MyPortfolioApp());
 }
